faa5a438424f389244be28206854bdd2293bfd38
Robert Hannen <robert_wilhelm.hannen@smail.th-koeln.de>
2025-01-20 14:39:50 +0000

    python arm publisher update

--- PCB_Detection_2/roslibpy-arm-test.py
+++ PCB_Detection_2/roslibpy-arm-test.py
@@ -1,9 +1,5 @@
 import roslibpy
 
-# https://docs.pal-robotics.com/tiago-single/handbook.html#jointcommander-plugin-configuration
-# https://docs.pal-robotics.com/tiago-single/handbook.html#arm-control-ros-api
-# https://docs.pal-robotics.com/tiago-single/handbook.html#controller-manager-services
-
 from dotenv import dotenv_values
 config = dotenv_values(".env")
 
@@ -12,4 +8,4 @@ client.run()
 
 topic = roslibpy.Topic(client, '/thk_ns/thk_tiago_xya', 'std_msgs/String')
 
-topic.publish({"x": 0.4, "y": 0.4, "angle": 0.0})
+topic.publish({"x": 0.4, "y": 0.6, "angle": 0.0})
