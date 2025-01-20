bcb7e1fbadd3a1afd19de7166d75f838b4488ec6
Robert Hannen <robert_wilhelm.hannen@smail.th-koeln.de>
2025-01-20 14:35:17 +0000

    added working thk arm controller publisher

--- PCB_Detection_2/roslibpy-arm-test.py
+++ PCB_Detection_2/roslibpy-arm-test.py
@@ -1,8 +1,4 @@
 import roslibpy
-from _THK_AMS_msg1 import THK_AMS_msg1
-import rospy
-from io import StringIO
-
 
 # https://docs.pal-robotics.com/tiago-single/handbook.html#jointcommander-plugin-configuration
 # https://docs.pal-robotics.com/tiago-single/handbook.html#arm-control-ros-api
@@ -16,26 +12,4 @@ client.run()
 
 topic = roslibpy.Topic(client, '/thk_ns/thk_tiago_xya', 'std_msgs/String')
 
-message = THK_AMS_msg1()
-message.x = 0.4
-message.y = 0.4
-message.angle = 0.0
-
-rospy.msg.serialize_message()
-
-#topic.publish({"x": 0.4, "y": 0.4, "angle": 0.0})
-topic.publish(message.serialize())
-
-"""
-topics = client.get_topics()
-topics.sort()
-for topic in topics:
-    if "arm" in topic:
-        print(topic)
-
-try:
-    while True:
-        pass
-except KeyboardInterrupt:
-    client.terminate()
-"""
+topic.publish({"x": 0.4, "y": 0.4, "angle": 0.0})
