586ab24fb454ebc14d70e56f895f9cb8dbf251da
Robert Hannen <robert_wilhelm.hannen@smail.th-koeln.de>
2025-02-10 13:07:35 +0100

    camera calibration output mean error

--- PCB_Detection_2/calibrate_camera.py
+++ PCB_Detection_2/calibrate_camera.py
@@ -79,6 +79,13 @@ for fname in images:
     with open(".env.json", "w") as f:
         json.dump(config_json, f, indent=2)
 
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+ 
+print( "total error: {}".format(mean_error/len(objpoints)) )
 
 while True:
     if cv.waitKey(1) & 0xFF == ord('q'):
